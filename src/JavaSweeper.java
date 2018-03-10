07afee8890cbcbf34cda6f0fb2b7457aac0a5705
artem <artemfap@yandex.ru>
2018-03-11 00:33:49 +0300

    fix JavaSweeper

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import Sweeper.Box;
 import Sweeper.Coord;
 import Sweeper.Game;
@@ -9,6 +12,7 @@ public class JavaSweeper extends JFrame{
 
     private Game game;
     private JPanel panel;
+    private JLabel label;
     private final int COLS = 9;
     private final int ROWS = 9;
     private final int BOMBS = 10;
@@ -23,8 +27,14 @@ public class JavaSweeper extends JFrame{
         game = new Game(COLS, ROWS, BOMBS);
         game.start();
         setImages();
+        initLabel();
         initPanel();
         initFrame();
+    }
+
+    private void initLabel() {
+        label = new JLabel("Welcome");
+        add(label, BorderLayout.SOUTH);
     }
 
     private void initPanel(){
@@ -37,20 +47,48 @@ public class JavaSweeper extends JFrame{
                             coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
             }
         };
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / IMAGE_SIZE; //получаем координату х мышкой
+                int y = e.getY() / IMAGE_SIZE; //получаем координату y мышкой
+                Coord coord = new Coord(x, y);
+                if (e.getButton() == MouseEvent.BUTTON1)
+                    game.pressLeftButton(coord);
+                if (e.getButton() == MouseEvent.BUTTON3)
+                    game.pressRightButton(coord);
+                if (e.getButton() == MouseEvent.BUTTON2)
+                    game.start();
+                label.setText(getMessage());
+                panel.repaint(); //перерисовывает панель
+
+            }
+        });
+
         panel.setPreferredSize(new Dimension(
                 Ranges.getSize().x * IMAGE_SIZE,
                 Ranges.getSize().y * IMAGE_SIZE));//устанавливает размер панели
         add(panel);
     }
 
+    private String getMessage() {
+        switch (game.getState()) {
+            case PLAYED: return "Open your mind";
+            case BOMBED: return "You Lose!";
+            case WINNER: return "You Winner!";
+            default: return null;
+        }
+    }
+
     private void initFrame(){
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//останавливать программу при нажатии крестика
         setTitle("Java Sweeper");//название окна
-        setLocationRelativeTo(null);//по центру
         setResizable(false);//неизменяемость окон
         setVisible(true);//сделать видимым весь фрейм
-        setIconImage(getImage("icon"));// иконка программы
         pack();
+        setLocationRelativeTo(null);//по центру
+        setIconImage(getImage("icon"));// иконка программы
     }
 
     private void setImages() {
